293ebe07eeb8d886965be4e2e4aec4e66eb84cd8
Sasanka Dahanayake <99157125+it21383366@users.noreply.github.com>
2026-05-10 01:05:14 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -258,7 +258,7 @@ app.get("/api/setup-database", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
